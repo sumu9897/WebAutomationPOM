64e4568fdb25a69fb417878a4dfc590fd70b0504
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-27 18:26:52 +0600

    Driver Setup

--- src/test/java/uitilities/DriverSetup.java
+++ src/test/java/uitilities/DriverSetup.java
@@ -2,6 +2,8 @@ package uitilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -18,5 +20,18 @@ public class DriverSetup {
     @AfterTest
     public void quiteBrowser(){
         browser.quit();
+    }
+
+    public WebDriver getBrowser(String browserName) {
+        if(browserName.equalsIgnoreCase("Chrome"))
+            return new ChromeDriver();
+        else if (browserName.equalsIgnoreCase("eEge"))
+            return new EdgeDriver();
+        else if (browserName.equalsIgnoreCase("Firefox"))
+            return new FirefoxDriver();
+        else {
+            throw new RuntimeException("Browser is not available with the given name: " + browserName);
+        }
+
     }
 }
