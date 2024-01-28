125324f569983bce8d99e38614247ef2748e24db
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-28 16:49:34 +0600

    Driver Setup

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static uitilities.DriverSetup.getBrowser;
+
 public class BasePage {
+
+    public WebElement getElement(By locator){
+
+        getBrowser().findElement(locator);
+
+    }
+
+    public void clickOnElement(By locator){
+        getElement(locator).click();
+    }
 }
--- src/test/java/testcases/TestLogin.java
+++ src/test/java/testcases/TestLogin.java
@@ -1,11 +1,20 @@
 package testcases;
 
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.SignInPage;
 import uitilities.DriverSetup;
 
 public class TestLogin extends DriverSetup {
+
+
+    HomePage homePage =new HomePage();
+    SignInPage signInPage = new SignInPage();
     @Test
     public void testLogin(){
+
+        getBrowser().get(homePage.homepageURL);
+        homePage.getElement(homePage.signInButton).click();
 
 
     }
